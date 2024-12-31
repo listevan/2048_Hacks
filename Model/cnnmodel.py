4b5fde121c72abeb8895a326f76a4304492596dc
Steven Li <listevan101@gmail.com>
2024-12-30 20:07:13 -0800

    running but kernel crash

--- Model/cnnmodel.py
+++ Model/cnnmodel.py
@@ -7,7 +7,7 @@ class DQN(nn.Module):
     def __init__(self):
         super(DQN, self).__init__()
         
-        self.model = nn.sequential(
+        self.model = nn.Sequential(
             # input 1 x 4 x 4 array
             nn.Conv2d(1, 5, kernel_size=3, padding=1, bias=False),
             nn.BatchNorm2d(5),
@@ -17,13 +17,15 @@ class DQN(nn.Module):
             nn.Conv2d(5, 10, kernel_size=3, padding=1, bias=False),
             nn.BatchNorm2d(10),
             nn.LeakyReLU(0.2, inplace=True),
-            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.MaxPool2d(kernel_size=2, stride=2)
             # state size 10 x 1 x 1
-            nn.Linear(10, 4),
-            nn.ReLU()
-            # state size 4 x 1 x1
         )
 
+        self.fc = nn.Linear(10, 4)
+        self.softmax = nn.Softmax(0)
+
     def forward(self, x):
-        x = self.model(x)
+        x = self.model(x).squeeze()
+        x = self.fc(x)
+        x = self.softmax(x)
         return torch.argmax(x) 
