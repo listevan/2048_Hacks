61bf035800205cd6f374486b76a1a3c11d5b9392
Steven Li <74156687+listevan@users.noreply.github.com>
2025-01-25 18:59:18 -0600

    training should be working

--- Game_Model/train_model.py
+++ Game_Model/train_model.py
@@ -20,37 +20,33 @@ from reward_func import get_reward
 from select_action import select_action
 from Game_Sim import game
 
-device = torch.device(
-    "cuda" if torch.cuda.is_available() else
-    "cpu"
-)
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print(device)
 
 # Create a ConfigParser object
 config = configparser.ConfigParser()
 
 # Read the configuration file
-config.read('Game_Model/config.ini')
+config.read("Game_Model/config.ini")
 
 # Access values from the configuration file
 EX_MODEL = bool(input("Train 2D CNN Model (0) or 3D CNN Model (1): "))
-BATCH_SIZE = config.getint('PARAMS', 'BATCH_SIZE')
-GAMMA = config.getfloat('PARAMS', 'GAMMA')
-EPS_START = config.getfloat('PARAMS', 'EPS_START')
-EPS_END = config.getfloat('PARAMS', 'EPS_END')
-EPS_DECAY = config.getint('PARAMS', 'EPS_DECAY')
-LR = config.getfloat('PARAMS', 'LR')
-TARGET_Q = config.getint('PARAMS', 'TARGET_Q')
-num_episodes = config.getint('PARAMS', 'num_episodes') # epochs
+BATCH_SIZE = config.getint("PARAMS", "BATCH_SIZE")
+GAMMA = config.getfloat("PARAMS", "GAMMA")
+EPS_START = config.getfloat("PARAMS", "EPS_START")
+EPS_END = config.getfloat("PARAMS", "EPS_END")
+EPS_DECAY = config.getint("PARAMS", "EPS_DECAY")
+LR = config.getfloat("PARAMS", "LR")
+TARGET_Q = config.getint("PARAMS", "TARGET_Q")
+num_episodes = config.getint("PARAMS", "num_episodes")  # epochs
 
 """
     ReplayMemory this keeps track of preiovus moves for training and is used to train ght emodel
 """
-Transition = namedtuple('Transition',
-                        ('state', 'action', 'reward'))
+Transition = namedtuple("Transition", ("state", "action", "reward"))
+
 
 class ReplayMemory(object):
-
     def __init__(self, capacity):
         self.memory = deque([], maxlen=capacity)
 
@@ -64,12 +60,13 @@ class ReplayMemory(object):
     def __len__(self):
         return len(self.memory)
 
-if not EX_MODEL: # 2D CNN
-    policy_net = DQN(11).to(device) # F(State, Action) -> Reward
-    target_net = DQN(11).to(device) # Maximize Reward
-else: # 3D CNN
-    policy_net = DQNex(1).to(device) # F(State, Action) -> Reward
-    target_net = DQNex(1).to(device) # Maximize Reward
+
+if not EX_MODEL:  # 2D CNN
+    policy_net = DQN(11).to(device)  # F(State, Action) -> Reward
+    target_net = DQN(11).to(device)  # Maximize Reward
+else:  # 3D CNN
+    policy_net = DQNex(1).to(device)  # F(State, Action) -> Reward
+    target_net = DQNex(1).to(device)  # Maximize Reward
 target_net.load_state_dict(policy_net.state_dict())
 
 optimizer = optim.AdamW(policy_net.parameters(), lr=LR, amsgrad=True)
@@ -78,6 +75,7 @@ memory = ReplayMemory(10000)
 steps_done = 0
 
 episode_durations = []
+
 
 def optimize_model():
     if len(memory) < BATCH_SIZE:
@@ -89,8 +87,8 @@ def optimize_model():
     reward_batch = torch.zeros((BATCH_SIZE, 4))
 
     next_state_values = torch.zeros((BATCH_SIZE, 4), device=device)
-    
-    for i in range(4): # get the next_state rewards for all possible next states for more state space exploration
+
+    for i in range(4):
         with torch.no_grad():
             next_states = torch.zeros((BATCH_SIZE, 1, 11, 4, 4))
             for j in range(BATCH_SIZE):
@@ -99,14 +97,25 @@ def optimize_model():
                 successful, combined_values, combined_indexes = g.move(i)
                 next_states[i, 0, :, :, :] = torch.tensor(g.board)
 
-                reward_batch[j, i] = get_reward(state_batch[j].squeeze(), i, g.board, g.check_win(), g.check_loss(), successful, combined_values, combined_indexes)
-            
+                reward_batch[j, i] = get_reward(
+                    state_batch[j].squeeze(),
+                    i,
+                    g.board,
+                    g.check_win(),
+                    g.check_loss(),
+                    successful,
+                    combined_values,
+                    combined_indexes,
+                )
+
             next_states = torch.tensor(next_states).to(device)
             next_state_values[:, i] = target_net(next_states).max(1).values
 
     expected_state_action_values = (next_state_values * GAMMA) + reward_batch
-    
-    state_action_values = policy_net(state)
+
+    state_action_values = policy_net(
+        torch.tensor(state_batch, dtype=torch.float32, device=device)
+    )
 
     # Compute MSE loss
     criterion = nn.MSELoss()
@@ -116,10 +125,13 @@ def optimize_model():
     optimizer.zero_grad()
     loss.backward()
     # # In-place gradient clipping
-    torch.nn.utils.clip_grad_value_(policy_net.parameters(), 100) # normal clipping was 100
+    torch.nn.utils.clip_grad_value_(
+        policy_net.parameters(), 100
+    )  # normal clipping was 100
     optimizer.step()
 
     return loss.item()
+
 
 model_loss = []
 
@@ -131,26 +143,43 @@ for i_episode in range(num_episodes):
     state = g.board
     # state = torch.tensor(state.flatten(), dtype=torch.float32, device=device).unsqueeze(0) # nn
     if not EX_MODEL:
-        state = torch.tensor(state, dtype=torch.float32, device=device).unsqueeze(0) # cnn
+        state = torch.tensor(state, dtype=torch.float32, device=device).unsqueeze(
+            0
+        )  # cnn
     else:
-        state = torch.tensor(state, dtype=torch.float32, device=device).unsqueeze(0).unsqueeze(0) # 3d cnn
+        state = (
+            torch.tensor(state, dtype=torch.float32, device=device)
+            .unsqueeze(0)
+            .unsqueeze(0)
+        )  # 3d cnn
 
     for t in count():
         prev = g.board
 
-        action, steps_done = select_action(prev, policy_net, steps_done, EPS_START, EPS_END, EPS_DECAY, EX_MODEL)
-        
+        action, steps_done = select_action(
+            prev, policy_net, steps_done, EPS_START, EPS_END, EPS_DECAY, EX_MODEL
+        )
+
         # print('move is', action,'type is', type(action), end = "\r")
         success, combined_values, combined_indexes = g.move(action)
         action = torch.tensor([action], dtype=torch.int64, device=device)
         observation = g.board
-        
+
         terminated = g.check_win()
         truncated = g.check_loss()
 
-        reward = get_reward(prev, action, observation, terminated, truncated, success, combined_values, combined_indexes)
+        reward = get_reward(
+            prev,
+            action,
+            observation,
+            terminated,
+            truncated,
+            success,
+            combined_values,
+            combined_indexes,
+        )
         # print('reward is', reward, 'won: ', terminated, 'lost: ', truncated, end="\r")
-        
+
         reward = torch.tensor([reward], dtype=torch.float32, device=device)
         done = terminated or truncated
 
@@ -159,12 +188,24 @@ for i_episode in range(num_episodes):
         else:
             # next_state = torch.tensor(observation.flatten(), dtype=torch.float32, device=device).unsqueeze(0)
             if not EX_MODEL:
-                next_state = torch.tensor(observation, dtype=torch.float32, device=device).unsqueeze(0) # cnn
+                next_state = torch.tensor(
+                    observation, dtype=torch.float32, device=device
+                ).unsqueeze(
+                    0
+                )  # cnn
             else:
-                next_state = torch.tensor(observation, dtype=torch.float32, device=device).unsqueeze(0).unsqueeze(0) # 3d cnn
+                next_state = (
+                    torch.tensor(observation, dtype=torch.float32, device=device)
+                    .unsqueeze(0)
+                    .unsqueeze(0)
+                )
+
+                # 3d cnn
 
         # Store the transition in memory
-        memory.push(np.expand_dims(np.expand_dims(prev, axis = 0), axis = 0), action, reward)
+        memory.push(
+            np.expand_dims(np.expand_dims(prev, axis=0), axis=0), action, reward
+        )
 
         # Move to the next state
         state = next_state
@@ -175,7 +216,7 @@ for i_episode in range(num_episodes):
 
         # Soft update of the target network's weights
         if steps_done % TARGET_Q == 0:
-            print('updating static network')
+            print("updating static network")
             policy_net_state_dict = policy_net.state_dict()
             target_net.load_state_dict(policy_net_state_dict)
 
@@ -183,7 +224,12 @@ for i_episode in range(num_episodes):
             episode_durations.append(t + 1)
             break
 
-    print('done with epoch #{}, max: {}, loss: {}, steps played: {}'.format(i_episode, g.max(), loss, episode_durations[-1]), end='\r')
+    print(
+        "done with epoch #{}, max: {}, loss: {}, steps played: {}".format(
+            i_episode, g.max(), loss, episode_durations[-1]
+        ),
+        end="\r",
+    )
 
 plt.plot(model_loss)
 plt.show()
@@ -192,16 +238,9 @@ plt.show()
 target_net_state_dict = target_net.state_dict()
 policy_net_state_dict = policy_net.state_dict()
 
-if not EX_MODEL: # 2D CNN
-    torch.save(target_net_state_dict, 'Model/saved_models/2dtarget_net.pt')
-    torch.save(policy_net_state_dict, 'Model/saved_models/2dpolicy_net.pt')
-else: # 3D CNN
-    torch.save(target_net_state_dict, 'Game_Model/Model/saved_models/2dtarget_net.pt')
-    torch.save(policy_net_state_dict, 'Game_Model/Model/saved_models/2dpolicy_net.pt')
-
-
-
-
-
-
-
+if not EX_MODEL:  # 2D CNN
+    torch.save(target_net_state_dict, "Model/saved_models/2dtarget_net.pt")
+    torch.save(policy_net_state_dict, "Model/saved_models/2dpolicy_net.pt")
+else:  # 3D CNN
+    torch.save(target_net_state_dict, "Game_Model/Model/saved_models/2dtarget_net.pt")
+    torch.save(policy_net_state_dict, "Game_Model/Model/saved_models/2dpolicy_net.pt")
